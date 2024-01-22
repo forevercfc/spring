5d406dc3fb8277a254444ed4f5f4af244423bd08
forevercfc <144869452+forevercfc@users.noreply.github.com>
2024-01-22 18:14:47 +0900

    Add files via upload

---
+++ SB07_Lombok/src/main/java/com/ex/springboot/ContentDTO.java
@@ -0,0 +1,10 @@
+package com.ex.springboot;
+
+import lombok.Data;
+
+@Data
+public class ContentDTO {
+	private int id;
+	private String Writer;
+	private String Conrtent;
+}
---
+++ SB07_Lombok/src/main/java/com/ex/springboot/Sb07LombokApplication.java
@@ -0,0 +1,13 @@
+package com.ex.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb07LombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb07LombokApplication.class, args);
+	}
+
+}
